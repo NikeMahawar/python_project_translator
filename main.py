bab011807c5c990bce21f97e0e5ea31bfba5eac3
Nikunj Mahawar <nikemahawar1824@gmail.com>
2023-06-17 16:29:12 +0530

    Second Commit

--- main.py
+++ main.py
@@ -2,31 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 from mtranslate import translate
 import webbrowser
-lang= input("English: 'en' \n"
-"Spanish: 'es' \n"
-"French: 'fr' \n"
-"German: 'de' \n"
-"Italian: 'it' \n"
-"Portuguese: 'pt' \n"
-"Russian: 'ru' \n"
-"Japanese: 'ja' \n"
-"Chinese (Simplified): 'zh-cn' "
-"Chinese (Traditional): 'zh-tw' "
-"Korean: 'ko' \n"
-"Arabic: 'ar' \n"
-"Dutch: 'nl' \n"
-"Swedish: 'sv' \n"
-"Danish: 'da' \n"
-"Norwegian: 'no' \n"
-"Finnish: 'fi' \n"
-"Greek: 'el' \n"
-"Turkish: 'tr' \n"
-"Please write the code of suitable language from the above: ")
-# Function to translate website
-def translate_website(file_path, target_language):
-    # Read the HTML file
-    with open(file_path, 'r', encoding='utf-8') as file:
-        html_content = file.read()
+
+lang = input("English: 'en' \n"
+             "Spanish: 'es' \n"
+             "French: 'fr' \n"
+             "German: 'de' \n"
+             "Italian: 'it' \n"
+             "Portuguese: 'pt' \n"
+             "Russian: 'ru' \n"
+             "Japanese: 'ja' \n"
+             "Chinese (Simplified): 'zh-cn' "
+             "Chinese (Traditional): 'zh-tw' "
+             "Korean: 'ko' \n"
+             "Arabic: 'ar' \n"
+             "Dutch: 'nl' \n"
+             "Swedish: 'sv' \n"
+             "Danish: 'da' \n"
+             "Norwegian: 'no' \n"
+             "Finnish: 'fi' \n"
+             "Greek: 'el' \n"
+             "Turkish: 'tr' \n"
+             "Please write the code of the suitable language from the above: ")
+
+def translate_website(url, target_language):
+    # Fetch the HTML content from the URL
+    response = requests.get(url)
+    html_content = response.text
 
     # Parse the HTML content
     soup = BeautifulSoup(html_content, 'html.parser')
@@ -43,16 +44,12 @@ def translate_website(file_path, target_language):
         translated_text = translate(original_text, target_language)
         element.string = translated_text
 
-    # Save the translated HTML to a file
+    # Get the translated HTML content
     translated_html = soup.prettify()
-    translated_file = "D:\\web_development\\translated.html"
-    with open(translated_file, "w", encoding="utf-8") as f:
-        f.write(translated_html)
 
-    # Open the translated file in Chrome
-    webbrowser.open('file://' + translated_file)
+    # Open the translated content in a new browser tab
+    webbrowser.open_new_tab(f"data:text/html;charset=utf-8,{translated_html}")
 
 # Usage example
-file_path = r'D:\web_development\my_website.html'
-translate_website(file_path, lang)
-
+url = input("Enter the URL of the website to translate: ")
+translate_website(url, lang)
